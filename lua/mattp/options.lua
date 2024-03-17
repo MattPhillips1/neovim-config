cf2cb67d753636ef9135bbed48a8a131a4c3035b
MattPhillips1 <matt.phillips121@gmail.com>
2024-03-18 10:29:29 +1100

    Add absolute number

--- lua/mattp/options.lua
+++ lua/mattp/options.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_set_option('rnu', true)
+vim.api.nvim_set_option('nu', true)
 vim.api.nvim_set_option('expandtab', true)
 vim.api.nvim_set_option('tabstop', 2)
 vim.api.nvim_set_option('softtabstop', 2)
