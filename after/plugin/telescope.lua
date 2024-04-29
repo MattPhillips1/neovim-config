4438b9f3363d5d4087ab17b10cdfabf37768ae1f
MattPhillips1 <matt.phillips121@gmail.com>
2024-04-29 14:38:52 +1000

    add marks telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -18,5 +18,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>f.', function () builtin.find_files({ cwd = require('telescope.utils').buffer_dir() }) end, {})
+vim.keymap.set('n', '<leader>fm', builtin.marks, {})
 
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
