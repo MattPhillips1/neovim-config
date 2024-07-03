87fb14c4802a5e8d1710bf59d6721730f7b0ac06
MattPhillips1 <matt.phillips121@gmail.com>
2024-07-03 22:09:23 +1000

    disable the fs monitor

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,1 +1,7 @@
-require('gitsigns').setup()
+require('gitsigns').setup({
+  watch_gitdir = {
+    -- Disable because watchman (https://facebook.github.io/watchman) updates the .git directory often
+    -- when using it as git.fsmonitor
+    enable = false,
+  },
+})
