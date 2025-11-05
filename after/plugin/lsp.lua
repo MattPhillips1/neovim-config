53a2ebd0813c02f9dba831677e24f757484efbc2
MattPhillips1 <matt.phillips121@gmail.com>
2025-11-05 21:36:13 +1100

    Remove copilot

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,7 @@
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {'jdtls', 'rust_analyzer', 'lua_ls', 'starpls', 'terraformls', 'zls'},
+  automatic_enable = false,
 })
 
 local lspconfig = require('lspconfig')
